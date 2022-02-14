f00481c42cbd0e3e78f08be5ced8305154e4cf8b
Piotr Roslaniec <p.roslaniec@gmail.com>
2022-02-14 23:11:32 +0100

    Custom TextEncoder implementation

--- aleo-snap/src/aleo.js
+++ aleo-snap/src/aleo.js
@@ -1,5 +1,8 @@
 const aleo = require('aleo-wasm');
 
+// const randomAddress = () => new aleo.Account().to_address();
+const randomAddress = () => "not-a-real-address";
+
 module.exports = {
-  randomAddress: new aleo.Account().to_address(),
+  randomAddress,
 };
